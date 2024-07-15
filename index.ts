39f3e995e57a9ad3680c1b76f1c5c40dbfb92a1a
Mahiro Takeshita <103738544+Mahiro-xi@users.noreply.github.com>
2024-07-15 21:31:04 +0900

    SEND GET AND RECEIVING

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+const hostname = Bun.argv[2];
+const port = Bun.argv[3];
+
+if (Bun.argv.length < 4) {
+    console.log("Usage: bun run index.ts <hostname> <port>");
+    process.exit(1);
+}else if(port.length > 5){
+    console.log("Wrong Arguments")
+    console.log("Usage: bun run index.ts <hostname> <port>")
+    process.exit(1);
+}
+
+const net = require('net');
+
+const client = new net.Socket();
+let recvmsg = "";
+client.connect(port, hostname, () => {
+    console.log(`Connected to ${hostname}:${port}`);
+    client.write("GET / HTTP/1.0\r\n\r\n");
+});
+
+client.on('data', (data: string) => {
+    recvmsg = data.toString();
+    console.log(recvmsg);
+    client.end();
+});
+
+
+client.on('end', () => {
+    console.log('Disconnected');
+});
